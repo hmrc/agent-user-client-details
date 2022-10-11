b9030ef601cb39899afec48e6492802df662b959
Unmesh Malvankar <unmesh.malvankar1@digital.hmrc.gov.uk>
2022-10-11 07:45:23 +0100

    [UM] Upgrades bootstrap-backend dependency

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,7 +6,7 @@ import sbt._
 object AppDependencies {
 
   val compile = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-backend-play-28"         % "7.3.0",
+    "uk.gov.hmrc"             %% "bootstrap-backend-play-28"         % "7.7.0",
     "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-28"                % "0.73.0",
     "uk.gov.hmrc.mongo"       %% "hmrc-mongo-work-item-repo-play-28" % "0.73.0",
     "uk.gov.hmrc"             %% "agent-mtd-identifiers"             % "0.47.0-play-28",
@@ -17,7 +17,7 @@ object AppDependencies {
   )
 
   val test = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-test-play-28"     % "7.3.0"         % "test, it",
+    "uk.gov.hmrc"             %% "bootstrap-test-play-28"     % "7.7.0"         % "test, it",
     "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-28"    % "0.73.0"        % "test, it",
     "org.scalamock"           %% "scalamock"                  % "5.1.0"         % "test, it",
     "com.vladsch.flexmark"    %  "flexmark-all"               % "0.36.8"        % "test, it"
