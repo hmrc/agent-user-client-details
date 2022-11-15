bc25b627cabe23475bb3a1885919e886da8d5303
Max Ashton <max.ashton@digital.hmrc.gov.uk>
2022-11-15 12:06:45 +0000

    APB-6649 first cut of a paginated endpoint for APFE

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -9,7 +9,7 @@ object AppDependencies {
     "uk.gov.hmrc"             %% "bootstrap-backend-play-28"         % "7.7.0",
     "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-28"                % "0.73.0",
     "uk.gov.hmrc.mongo"       %% "hmrc-mongo-work-item-repo-play-28" % "0.73.0",
-    "uk.gov.hmrc"             %% "agent-mtd-identifiers"             % "0.47.0-play-28",
+    "uk.gov.hmrc"             %% "agent-mtd-identifiers"             % "0.48.0-play-28",
     "uk.gov.hmrc"             %% "cluster-work-throttling"           % "8.3.0-play-28",
     "uk.gov.hmrc"             %% "agent-kenshoo-monitoring"          % "4.8.0-play-28",
     "com.github.blemale"      %% "scaffeine"                         % "4.0.1",
