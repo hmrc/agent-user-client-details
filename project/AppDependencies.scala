b26e36dd7e44e1b99cc496e74738eaf7a2e9a226
mrolfe88 <52818407+mrolfe88@users.noreply.github.com>
2025-08-18 11:14:40 +0100

    [APB-8315] Bump dependency versions

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,14 +3,14 @@ import sbt.*
 object AppDependencies {
 
   private val bootstrapVer: String = "9.19.0"
-  private val mongoVer: String = "2.6.0"
+  private val mongoVer: String = "2.7.0"
   private val pekkoVersion = "1.0.3"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc.mongo"       %% "hmrc-mongo-work-item-repo-play-30" % mongoVer,
     "uk.gov.hmrc"             %% "bootstrap-backend-play-30"         % bootstrapVer,
     "uk.gov.hmrc"             %% "cluster-work-throttling"           % "9.2.0",
-    "uk.gov.hmrc"             %% "crypto-json-play-30"               % "8.2.0",
+    "uk.gov.hmrc"             %% "crypto-json-play-30"               % "8.3.0",
     "uk.gov.hmrc"             %% "domain-play-30"                    % "11.0.0"
   )
 
